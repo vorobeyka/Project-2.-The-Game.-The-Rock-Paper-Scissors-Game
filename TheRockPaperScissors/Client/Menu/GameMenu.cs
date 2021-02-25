53a726d97b854acc1fe8eb929194911b4407b55a
emvoro <emvoro@gmail.com>
2021-02-25 12:45:44 +0200

    gamemenu need to save

--- TheRockPaperScissors/Client/Menu/GameMenu.cs
+++ TheRockPaperScissors/Client/Menu/GameMenu.cs
@@ -11,7 +11,6 @@ namespace TheRockPaperScissors.Client.Menu
     {
         private readonly MenuDesign _menuDesign = new MenuDesign();
         private readonly MenuValidation _menuValidation = new MenuValidation();
-        //private readonly GameModes _gameModes = new GameModes();
         private readonly GameTypeService _gameTypeService = new GameTypeService();
 
         public async Task Load(ConsoleColor color, User user)
@@ -30,43 +29,32 @@ namespace TheRockPaperScissors.Client.Menu
 
             Console.WriteLine($" {number} - Exit");
             int command = _menuValidation.CheckInteger(" Enter number ", number);
-            //GameResult result = new GameResult();
 
             switch (command)
             {
                 case 1:
-                    /* result =*/ await _gameTypeService.CreateGame(user.Id, GameType.Test);
-                     break;
-                 case 2:
-                     int privateCommand = _menuValidation.CheckInteger("\n 1 - Create room\n 2 - Connect to room", 2);
-                     switch (privateCommand)
-                     {
-                         case 1:
-                             /* result =*/
-                    await _gameTypeService.CreateGame(user.Id, GameType.Private);
+                    await _gameTypeService.CreateGame(user.Id, GameType.Test);
+                    break;
+                case 2:
+                    int privateCommand = _menuValidation.CheckInteger("\n 1 - Create room\n 2 - Connect to room", 2);
+                    switch (privateCommand)
+                    {
+                        case 1:
+                            await _gameTypeService.CreateGame(user.Id, GameType.Private);
                             break;
                         case 2:
-                            /* result =*/
                             string room = _menuValidation.InputString(" Enter room identifier :");
                             await _gameTypeService.ConnectToPrivate(user.Id, GameType.Private, room);
                             break;
                     }
-                   
                     break;
                 case 3:
-                  /*  result = */await _gameTypeService.CreateGame(user.Id, GameType.Public);
+                    await _gameTypeService.CreateGame(user.Id, GameType.Public);
                     break;
                 case 4:
                     Console.Clear();
                     break;
             }
         }
-
-        //public async Task/*<GameResult>*/ Play(Guid player, GameType gametype)
-        //{
-        //    Console.Clear();
-        //    await _gameTypeService.PlayTest(player, gametype);
-        //   // return new GameResult();
-        //}
     }
 }
